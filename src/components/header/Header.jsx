d35d78439a2af0626a7c338e1438ad491ee0ef5d
leen <leenodeh287@gmail.com>
2023-12-17 12:04:51 +0300

    Movix_Project_Done

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,10 @@
-import  { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { HiOutlineSearch } from "react-icons/hi";
 import { SlMenu } from "react-icons/sl";
 import { VscChromeClose } from "react-icons/vsc";
 import { useNavigate, useLocation } from "react-router-dom";
 
-import "./Header.scss";
+import "./style.scss";
 
 import ContentWrapper from "../contentWrapper/ContentWrapper";
 import logo from "../../assets/movix-logo.svg";
