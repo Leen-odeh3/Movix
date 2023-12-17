31b06fe527a2e6c2d42d65b64a85221e72e2f0a0
leen <leenodeh287@gmail.com>
2023-12-17 12:00:23 +0300

    Pages Project

--- src/Pages/Home/heroBanner/HeroBanner.jsx
+++ src/Pages/Home/heroBanner/HeroBanner.jsx
@@ -1,7 +1,7 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
-import "./Style.scss";
+import "./style.scss";
 
 import useFetch from "../../../hooks/useFetch";
 
